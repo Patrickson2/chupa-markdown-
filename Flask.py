4a0438503c654f9c6f0d3ce26abae71ecc77942c
Patrickson2 <thairupatrickson@gmail.com>
2025-12-29 09:01:57 +0300

    Created landing page route

--- Flask.py
+++ Flask.py
@@ -7,8 +7,13 @@ app = Flask(__name__) #this is the constructor of the flask class
 # Routing - is the association of URLs and the code that should execute when a request comes in for that URL.
 # The easiest way to define routes with Flask is through use of the (@app.route) decorator:
 
-@app.route('/') #this is the routing point for the application 
-def hello_world():
+
+@app.route('/') #this is the landing page of the application
+def index(): #showcases that i am entering the home page 
+    return '<h1>Welcome Home</h1>'
+
+@app.route('/about') #this is the routing point for the application 
+def about():
     return 'Hello, World!'
 
 if __name__ == '__main__':
