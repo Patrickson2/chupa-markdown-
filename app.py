b3d4d82ce3c42d30fc40c7fe8abc477f03842026
Patrickson2 <thairupatrickson@gmail.com>
2025-12-31 01:58:02 +0300

    shocased a simple database configuration

--- app.py
+++ app.py
@@ -1,5 +1,21 @@
 # i am creating my first Flask application and also showcasing on the routing 
 from flask import Flask # i am importing the flask class
+from flask_migrate import Migrate #this is the migration instance
+from models import db #this is the database instance
+#create an instance of a class
+app = Flask(__name__) #this is the constructor of the flask class
+
+# Database configuration
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///school.db' #this is the configuration of the database
+
+migrate = Migrate(app, db)
+
+# now we initialize flask app to use db
+db.init_app(app)
+
+# Routing - is the association of URLs and the code that should execute when a request comes in for that URL.
+# The easiest way to define routes with Flask is through use of the (@app.route) decorator:
+
 
 @app.route('/') #this is the landing page of the application
 def index(): #showcases that i am entering the home page 
