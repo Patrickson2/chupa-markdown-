d0c10db8cec34c5668173501093a3fd8f99e7db3
Patrickson2 <thairupatrickson@gmail.com>
2025-12-31 03:08:20 +0300

    updated changes to make users visible

--- models.py
+++ models.py
@@ -8,17 +8,19 @@ class User(db.Model):# this indicates the database to be named as users
     __tablename__ ='users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String)
+    email = db.Column(db.String)
     password = db.Column(db.String)
     
-### i will create classes while trying to make the e-commerce web
+    def __repr__(self):
+        return f'<User {self.id}, {self.username}, {self.email}>'
 
-# class Post(db.Model): # this indicates the database to be named as posts
-#     __tablename__ ='posts'
-#     id = db.Column(db.Integer, primary_key=True)
-#     title = db.Column(db.String)
-#     content = db.Column(db.String)
-#     user_id = db.Column(db.Integer, db.ForeignKey('users.id')) # this is the foreign key constraint
-#     user = db.relationship('User', backref=db.backref('posts', lazy=True))
+### i will create classes while trying to make the e-commerce
+
+class Post(db.Model): # this indicates the database to be named as posts
+    __tablename__ ='posts'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String)
+    description = db.Column(db.String(250))
 
 
 
