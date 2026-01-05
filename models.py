e17a02e466e077d1cd144f5b43050c5c648c3016
Patrickson2 <thairupatrickson@gmail.com>
2026-01-05 14:59:51 +0300

    made a one-many relations code

--- models.py
+++ models.py
@@ -10,6 +10,9 @@ class User(db.Model):# this indicates the database to be named as users
     username = db.Column(db.String)
     email = db.Column(db.String)
     password = db.Column(db.String)
+
+    # One user can have as many Posts as they want
+    posts = db.relationship('Post', back_populates='user')
     
     def __repr__(self):
         return f'<User {self.id}, {self.username}, {self.email}>'
@@ -21,6 +24,12 @@ class Post(db.Model): # this indicates the database to be named as posts
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String)
     description = db.Column(db.String(250))
+    # now i want to create the SQLAlchemy relationship where one user has many posts.
+    # and each foreign key links a post to a user
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id')) 
+
+    # each post belongs to one user
+    user = db.relationship('User', back_populates='posts')
 
 
 
